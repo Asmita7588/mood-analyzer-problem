56047739992a99f7059d27fe5035070278d46d11
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-12-05 17:56:44 +0530

    Added: given null mood should throw mood analysis exception

--- src/test/java/org/example/MoodAnalyserTest.java
+++ src/test/java/org/example/MoodAnalyserTest.java
@@ -3,10 +3,12 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MoodAnalyserTest {
 
     @Test
-    public void givenSadMessage_shouldReturnSad() throws InvalidMoodException {
+    public void givenSadMessage_shouldReturnSad() throws MoodAnalysisException {
         MoodAnalyzer moodAnalyser = new MoodAnalyzer("SAD");
         String result = moodAnalyser.analyseMood("SAD");
         assertEquals("SAD", result);
@@ -14,7 +16,7 @@ public class MoodAnalyserTest {
     }
 
     @Test
-    public void givenAnyMessage_checkForSad_shouldReturnHappy() throws InvalidMoodException {
+    public void givenAnyMessage_checkForSad_shouldReturnHappy() throws MoodAnalysisException {
         MoodAnalyzer moodAnalyser = new MoodAnalyzer();
         String result = moodAnalyser.analyseMood("Happy");
             assertEquals("Happy", result);
@@ -23,10 +25,21 @@ public class MoodAnalyserTest {
     }
 
     @Test
-    public void givenNullMessage_ShouldReturnHappy() throws InvalidMoodException {
+    public void givenNullMessage_ShouldReturnHappy() throws MoodAnalysisException {
         MoodAnalyzer moodAnalyser = new MoodAnalyzer("Happy");
         String message = null;
         String result = moodAnalyser.analyseMood(message);
         assertEquals("Happy", result);
+    }
+
+    @Test
+    public void givenNullMessage_ShouldThrowMoodAnalysisException() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+        String message = null;
+        Exception exception = assertThrows(MoodAnalysisException.class, () -> {
+            moodAnalyser.analyseMood(message);
+        });
+        assertEquals(MoodAnalysisError.NULL_MOOD, ((MoodAnalysisException) exception).getError());
+        assertEquals("Mood cannot be null", exception.getMessage());
     }
 }
